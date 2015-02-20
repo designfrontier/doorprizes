13a69e1149687cdefd102fa0e690ce9538ec3255
Daniel Sellers <daniel@designfrontier.net>
2015-02-19 21:08:42 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# eventbrite-doorprize
+------
+
+doorprizes for checked in attendees at an eventbrite event
+
+------
+
+Built with [monumentjs](http://monument.ansble.com)
---
+++ routes/about.js
@@ -0,0 +1,14 @@
+var events = require('monument').events;
+
+
+events.on('route:/about:get', function (connection) {
+	'use strict';
+	
+	connection.res.end('route /about now responding to get requests');
+});
+
+events.on('route:/about:put', function (connection) {
+	'use strict';
+
+	connection.res.end('route /about now responding to put requests');
+});
