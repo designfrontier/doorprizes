ffa60e613946d2681db2d7e6ac8b318de43357b1
Daniel Sellers <daniel@designfrontier.net>
2015-02-21 09:15:23 -0700

    updated routes

---
+++ routes/random.js
@@ -0,0 +1,8 @@
+var events = require('monument').events;
+
+
+events.on('route:/random/:eventid:get', function (connection) {
+	'use strict';
+	
+	connection.res.end('route /random/:eventid now responding to get requests');
+});
