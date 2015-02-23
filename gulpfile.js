f020bd6e57087ddfc401815cfe4b683eb9e5ecf2
Daniel Sellers <daniel@designfrontier.net>
2015-02-23 11:23:21 -0700

    got the base styles knocked out

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,49 @@
-var gulp = require('gulp');
+var gulp = require('gulp')
+	, sass = require('gulp-sass')
+	, uglify = require('gulp-uglify')
+	, sourcemaps = require('gulp-sourcemaps')
 
-gulp.task('default', function () {
+	, jsFiles = [
+					'./public/components/default.js'
+					, './public/components/**/*.js'
+					, '!./public/components/**/*_test.js'
+				]
+	, scssFiles = [
+					'./public/components/default.scss'
+					, './public/components/**/*.scss'
+				];
+
+gulp.task('default', ['scss', 'js'], function () {
+
+});
+
+gulp.task('test', function () {
 
 });
 
 gulp.task('scss', function () {
-	// gulp.src('')
+	'use strict';
+
+	return gulp.src([
+				'./public/components/default.scss'
+			])
+			.pipe(sourcemaps.init())
+			.pipe(sass())
+		  	.pipe(sourcemaps.write('./maps'))
+			.pipe(gulp.dest('./public/dist'));
+});
+
+gulp.task('js', function () {
+	'use strict';
+
+	return gulp.src(jsFiles)
+			.pipe(uglify())
+			.pipe(gulp.dest('./public/dist/'));
+});
+
+gulp.task('dev', function () {
+	'use strict';
+
+	gulp.watch(jsFiles, ['js']);
+	gulp.watch(scssFiles, ['scss']);
 });
